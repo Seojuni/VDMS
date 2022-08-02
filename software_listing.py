ced33fa35b78577751930f98f3535e66ba9bb1b5
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-02 09:36:42 +0900

    Append if data already exists

--- software_listing.py
+++ software_listing.py
@@ -13,35 +13,57 @@ def software_listing():
     f.close()
 
 
-def insert_data():
-    conn = db_conn.db_conn()
-    cur = conn.cursor()
-    query = 'insert into software_list(name, version) values (?, ?)'
+def update_data(conn, cur):
+    insert_query = 'insert into software_list(name, version) values (?, ?)'
+    select_query = 'select count(*) from software_list'
 
     f = open('output.txt', 'r')
     line = f.readlines()
-    for l in line[4:-2]:
-        print(l.strip().split(' '))
-        if " " in l.strip().split(' '):
-            software = " ".join(l.strip().split(' ')[:-2]).rstrip()
-        else:
-            software = " ".join(l.strip().split(' ')[:-1]).rstrip()
-        print("software : " + software)
-        version = l.strip().split(' ')[-1]
-        print("version : " + version)
-        print(" ")
+    file_len = len(line) - 6
+    try:
+        cur.execute(select_query)
+    except mariadb.Error as e:
+        print(f"Error: {e}")
+    row = cur.fetchone()[0]
 
-        try:
-            cur.execute(query, (software, version))
-            conn.commit()
-        except mariadb.Error as e:
-            print(f"Error: {e}")
+    if file_len == row:
+        return
+    elif row == 0:
+        for l in line[4:-2]:
+            if " " in l.strip().split(' '):
+                software = " ".join(l.strip().split(' ')[:-2]).rstrip()
+            else:
+                software = " ".join(l.strip().split(' ')[:-1]).rstrip()
+            version = l.strip().split(' ')[-1]
 
-    conn.close()
+            try:
+                cur.execute(insert_query, (software, version))
+                conn.commit()
+            except mariadb.Error as e:
+                print(f"Error: {e}")
+    else:
+        for l in line[row+1:-2]:
+            if " " in l.strip().split(' '):
+                software = " ".join(l.strip().split(' ')[:-2]).rstrip()
+            else:
+                software = " ".join(l.strip().split(' ')[:-1]).rstrip()
+            version = l.strip().split(' ')[-1]
+
+            try:
+                cur.execute(insert_query, (software, version))
+                conn.commit()
+            except mariadb.Error as e:
+                print(f"Error: {e}")
+
     f.close()
     os.remove('output.txt')
 
 
 if __name__ == "__main__":
+    conn = db_conn.db_conn()
+    cur = conn.cursor()
+
     software_listing()
-    insert_data()
+    update_data(conn, cur)
+
+    conn.close()
