aa335498d5fe3855d91d69634e2abf2d9f08bdb4
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-06 13:27:42 +0900

    Update PC-03.bat

--- script/Windows PC/PC-03.bat
+++ script/Windows PC/PC-03.bat
@@ -1,1 +1,18 @@
+@ECHO OFF
+rem[PC-03] (상) 2. 서비스 관리 >.3 공유 폴더 제거
+secedit /EXPORT /CFG LocalSeurityPoilicy.txt
 
+TYPE LocalSeurityPoilicy.txt | findstr "C$ D$ Admin$" > share_folder.txt
+
+rem 1 : 양호 /2: 취약
+IF EXIST "C:\share_folder\" (
+    net share /delete C$
+    net share /delete D$
+    net share /delete Admin$
+        ECHO PC-03 1 >> result.txt
+        )
+else (
+        ECHO PC-03 2 >> result.txt
+)
+
+DEL LocalSeurityPoilicy.txt
