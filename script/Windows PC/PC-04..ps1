88ab34bdc9608a37cf82ba1b0ceeb1e5393d4d3a
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-06 15:03:17 +0900

    Add files via upload

---
+++ script/Windows PC/PC-04..ps1
@@ -0,0 +1,19 @@
+@ECHO OFF
+REM[PC-04] (상) 2.서비스 관리 > 2.2 불필요한 서비스 제거
+
+secedit /EXPORT /CFG LocalSeurityPoilicy.txt
+
+TYPE LocalSecurityPolicy.txt | find -i "Unnecessary_service" > uncecessary_service.txt
+
+
+rem 1 : 양호 /2: 취약
+
+IF %Unnecessary_service% NEQ 1(
+    ECHO PC-04 1 >> result.txt
+
+)ELSE(
+    ECHO PC-04 2 >> result.txt
+)
+
+DEL LocalSecurityPolicy.txt
+DEL Unnecessary_service.txt 
