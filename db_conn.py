23f90147a1e46a7afb04e5096c5ddc687c3f8dea
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-07 16:35:58 +0900

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -17,7 +17,7 @@ def db_conn():
                 user="root",
                 password="kisec123",
                 host="localhost",
-                port=3306,
+                port=3306
             )
             with conn:
                 with conn.cursor() as cur:
