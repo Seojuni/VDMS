8fb2a5b35b1feaea946395f754ac3df9e43b2371
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-07 16:35:44 +0900

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -11,8 +11,20 @@ def db_conn():
             port=3306,
             database="vdm"
         )
-    except mariadb.Error as e:
-        print(f"Error connecting to MariaDB Platform: {e}")
-        sys.exit(1)
+    except mariadb.Error:
+        try:
+            conn = mariadb.connect(
+                user="root",
+                password="kisec123",
+                host="localhost",
+                port=3306,
+            )
+            with conn:
+                with conn.cursor() as cur:
+                    cur.execute('CREATE DATABASE vdm')
+                    conn.commit()
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
 
     return conn
