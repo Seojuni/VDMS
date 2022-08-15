45731774a789a0bd98a287026ac2f470ce845bd3
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-15 14:34:53 +0900

    Update PC-09.bat

--- script/Windows PC/PC-09.bat
+++ script/Windows PC/PC-09.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM[PC-09] (상) 4. 보안관리 > 4.1 바이러스 백신 프로그램 설치 및 주기적 업데이트
 
-secedit /EXPORT /CFG LocalSeurityPoilicy.txt
+secedit /EXPORT /CFG LocalSeurityPolicy.txt
 
 TYPE LocalSecurityPolicy.txt | find -i "INSTALLING_VACCINE" >> vaccine.txt
 TYPE LocalSecurityPolicy.txt | find -i "LATEST_UPDATES" >> updates.txt
@@ -17,6 +17,6 @@ IF %INSTALLING_VACCINE% EQU 1(
  ECHO PC-09 2 >> result.txt
 )
 
-DEL LocalSeurityPoilicy.txt
+DEL LocalSeurityPolicy.txt
 DEL vaccine.txt
 DEL updates.txt
