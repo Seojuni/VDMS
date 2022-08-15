c83c42eb13219fb43ffc79d8bc9e90ce4a4b1b8d
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-15 20:05:16 +0900

    Update cve_crawler.py

--- cve_crawler.py
+++ cve_crawler.py
@@ -14,9 +14,12 @@ def insert_data(cur, vuln_id, pub_date, last_mod_date, description, score, softw
     except mariadb.Error as e:
         print(f"Error: {e}")
 
+
 def get_data():
     conn = db_conn.db_conn()
     cur = conn.cursor()
+
+    select_query = """select count(*) from cve_list where vuln_id = %s"""
 
     page_req = requests.get('https://nvd.nist.gov/vuln/search/results?isCpeNameSearch=false&cvss_version=2&results_type=overview&form_type=Advanced&search_type=all&startIndex=0')
 
@@ -39,65 +42,83 @@ def get_data():
                         continue
                     
                     vuln_row = page_soup.find('a', attrs={'data-testid': 'vuln-detail-link-'+str(i)}).get_text()
-                    
-                    detail_req = requests.get('https://nvd.nist.gov/vuln/detail/'+vuln_row)
 
-                    if detail_req.ok:
-                        detail_html = detail_req.text
-                        detail_soup = BeautifulSoup(detail_html, 'html.parser')
+                    try:
+                        cur.execute(select_query, [vuln_row])
+                    except mariadb.Error as e:
+                        print(f"Error: {e}")
+                    is_data = cur.fetchone()
 
-                        pub_date = detail_soup.find('span', attrs={'data-testid': 'vuln-published-on'}).get_text()
+                    if is_data[0] == 0:
+                        detail_req = requests.get('https://nvd.nist.gov/vuln/detail/' + vuln_row)
 
-                        last_mod_date = detail_soup.find('span', attrs={'data-testid': 'vuln-last-modified-on'}).get_text()
+                        if detail_req.ok:
+                            detail_html = detail_req.text
+                            detail_soup = BeautifulSoup(detail_html, 'html.parser')
 
-                        description = detail_soup.find('p', attrs={'data-testid': 'vuln-description'}).get_text()
+                            pub_date = detail_soup.find('span', attrs={'data-testid': 'vuln-published-on'}).get_text()
 
-                        if detail_soup.find('a', attrs={'id': 'Cvss3NistCalculatorAnchorNA'}):
-                            score = detail_soup.find('a', attrs={'id': 'Cvss2CalculatorAnchor'}).get_text().split(' ')[0]
-                        else:
-                            if detail_soup.find('a', attrs={'id': 'Cvss3NistCalculatorAnchor'}) is None:
-                                score = detail_soup.find('a', attrs={'id': 'Cvss3CnaCalculatorAnchor'}).get_text().split(' ')[0]
+                            last_mod_date = detail_soup.find('span',
+                                                             attrs={'data-testid': 'vuln-last-modified-on'}).get_text()
+
+                            description = detail_soup.find('p', attrs={'data-testid': 'vuln-description'}).get_text()
+
+                            if detail_soup.find('a', attrs={'id': 'Cvss3NistCalculatorAnchorNA'}):
+                                score = \
+                                detail_soup.find('a', attrs={'id': 'Cvss2CalculatorAnchor'}).get_text().split(' ')[0]
                             else:
-                                score = detail_soup.find('a', attrs={'id': 'Cvss3NistCalculatorAnchor'}).get_text().split(' ')[0]
-                        if score == 'N/A':
-                            score = detail_soup.find('a', attrs={'id': 'Cvss2CalculatorAnchor'}).get_text().split(' ')[0]
-                        
-                        cwe_id = detail_soup.find('td', attrs={'data-testid': 'vuln-CWEs-link-0'}).get_text()
+                                if detail_soup.find('a', attrs={'id': 'Cvss3NistCalculatorAnchor'}) is None:
+                                    score = \
+                                    detail_soup.find('a', attrs={'id': 'Cvss3CnaCalculatorAnchor'}).get_text().split(
+                                        ' ')[0]
+                                else:
+                                    score = \
+                                    detail_soup.find('a', attrs={'id': 'Cvss3NistCalculatorAnchor'}).get_text().split(
+                                        ' ')[0]
+                            if score == 'N/A':
+                                score = \
+                                detail_soup.find('a', attrs={'id': 'Cvss2CalculatorAnchor'}).get_text().split(' ')[0]
 
-                        cwe_name = detail_soup.select('div#vulnTechnicalDetailsDiv > table > tbody > tr > td')[1].get_text()
+                            cwe_id = detail_soup.find('td',
+                                                      attrs={'data-testid': 'vuln-CWEs-link-0'}).get_text().strip()
 
-                        cpe_data = detail_soup.find('input', attrs={'id': 'cveTreeJsonDataHidden'}).get('value')
-                        
-                        start = cpe_data.find('"cpe22Uri"')
-                        end = cpe_data.find('","status"')
-                        cpe_code = cpe_data[start + 12:end]
-                        vendor = cpe_utils.CPE(cpe_code).get_human("vendor")
-                        product = cpe_utils.CPE(cpe_code).get_human("product")
-                        if product in vendor or vendor in product:
-                            software = product
-                        else:
-                            software = vendor + " " + product
+                            cwe_name = detail_soup.select('div#vulnTechnicalDetailsDiv > table > tbody > tr > td')[
+                                1].get_text()
 
-                        start = cpe_data.find('"rangeStartVersion"')
-                        end = cpe_data.find(',"rangeEndType"')
-                        rangeStartVersion = cpe_data[start + 20:end]
-                        if rangeStartVersion[0] == '"':
-                            rangeStartVersion = rangeStartVersion[1:-1]
+                            cpe_data = detail_soup.find('input', attrs={'id': 'cveTreeJsonDataHidden'}).get('value')
 
-                        start = cpe_data.find('"rangeEndVersion"')
-                        end = cpe_data.find(',"rangeId"')
-                        rangeEndVersion = cpe_data[start + 18:end]
-                        if rangeEndVersion[0] == '"':
-                            rangeEndVersion = rangeEndVersion[1:-1]
+                            start = cpe_data.find('"cpe22Uri"')
+                            end = cpe_data.find('","status"')
+                            cpe_code = cpe_data[start + 12:end]
+                            vendor = cpe_utils.CPE(cpe_code).get_human("vendor")
+                            product = cpe_utils.CPE(cpe_code).get_human("product")
+                            if product in vendor or vendor in product:
+                                software = product
+                            else:
+                                software = vendor + " " + product
 
-                        if rangeStartVersion == '' and rangeEndVersion == '':
-                            #rangeStartVersion = cpe_utils.CPE(cpe_code).get_human("version")
-                            continue
+                            start = cpe_data.find('"rangeStartVersion"')
+                            end = cpe_data.find(',"rangeEndType"')
+                            rangeStartVersion = cpe_data[start + 20:end]
+                            if rangeStartVersion[0] == '"':
+                                rangeStartVersion = rangeStartVersion[1:-1]
 
-                        insert_data(cur, vuln_row, pub_date, last_mod_date, description, score, software, rangeStartVersion, rangeEndVersion, cwe_id, cwe_name)
-                        conn.commit()
+                            start = cpe_data.find('"rangeEndVersion"')
+                            end = cpe_data.find(',"rangeId"')
+                            rangeEndVersion = cpe_data[start + 18:end]
+                            if rangeEndVersion[0] == '"':
+                                rangeEndVersion = rangeEndVersion[1:-1]
+
+                            if rangeStartVersion == '' and rangeEndVersion == '':
+                                # rangeStartVersion = cpe_utils.CPE(cpe_code).get_human("version")
+                                continue
+
+                            insert_data(cur, vuln_row, pub_date, last_mod_date, description, score, software,
+                                        rangeStartVersion, rangeEndVersion, cwe_id, cwe_name)
+                            conn.commit()
 
     conn.close()
+
 
 if __name__ == "__main__":
     get_data()
