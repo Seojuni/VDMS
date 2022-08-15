4f0ba6334c3ebe929f14a52a088326c40ca4b502
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-15 14:35:24 +0900

    Update PC-10.bat

--- script/Windows PC/PC-10.bat
+++ script/Windows PC/PC-10.bat
@@ -1,7 +1,7 @@
 @ECHO OFF 
 REM [PC-10] (상) 4.보안관리> 바이러스 백신 프로그램에서 제공하는 실시간 감시 기능 
 활성화
-secedit /EXPORT /CFG LocalSeurityPoilicy.txt
+secedit /EXPORT /CFG LocalSeurityPolicy.txt
 
 TYPE LocalSeurityPoilicy.txt | find -i "REAL_TIME_INSPECTION" >> inspection.txt
 
@@ -13,5 +13,5 @@ IF %REAL_TIME_INSPECTION% EQU 1(
  ECHO PC-09 2 >> result.txt
 )
 
-DEL LocalSeurityPoilicy.txt
+DEL LocalSeurityPolicy.txt
 DEL inspection.txt
