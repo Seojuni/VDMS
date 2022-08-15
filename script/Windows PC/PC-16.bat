05e1d544959e4e77a5e792820dd0ce9a294c83ea
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-15 14:37:18 +0900

    Update PC-16.bat

--- script/Windows PC/PC-16.bat
+++ script/Windows PC/PC-16.bat
@@ -2,7 +2,7 @@
 
 REM[PC-16] (중) 2.서비스 관리 > 2.4 파일 시스템을 NTFS 포맷으로 설정
 
-secedit /EXPORT /CFG LocalSeurityPoilicy.txt
+secedit /EXPORT /CFG LocalSeurityPolicy.txt
 TYPE LocalSecurityPolicy.txt | find -i "FILE_SYSTEM_NTFS" >> NTFS.txt
 TYPE LocalSecurityPolicy.txt | find -i "FILE_SYSTEM_FAT32" >> FAT32.txt
 
@@ -10,10 +10,10 @@ TYPE LocalSecurityPolicy.txt | find -i "FILE_SYSTEM_FAT32" >> FAT32.txt
 rem 1 : 양호 /2: 취약
 
 IF %FILE_SYSTEM_NTFS% EQU 1(
-         ECHO PC-16 1 result.txt
+         ECHO PC-16 1 >> result.txt
 )ELSE(
      %FILE_SYSTEM_FAT32(
-        ECHO PC-16 2 result.txt
+        ECHO PC-16 2 >>  result.txt
     )
 )
 
