9d54cbeb955d0b27f211408e20dea72fadedb547
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-15 14:36:13 +0900

    Update PC-13.bat

--- script/Windows PC/PC-13.bat
+++ script/Windows PC/PC-13.bat
@@ -2,7 +2,7 @@
 
 REM[PC-13] (상) 4. 보안관리 > 4.5 CD, DVD, USB메모리 등과 같은 미디어의 자동실행 방지 등 이동식 미디어에 대한 보안대책 수립
 
-secedit /EXPORT /CFG LocalSeurityPoilicy.txt
+secedit /EXPORT /CFG LocalSeurityPolicy.txt
 TYPE LocalSecurityPolicy.txt | find -i "AUTOMATIC_MEDIA" >> auto_media.txt
 TYPE LocalSecurityPolicy.txt | find -i "MANAGEMENT_PROCEDURE" >>management.txt
 
@@ -16,7 +16,7 @@ IF %AUTOMATIC_MEDIA% NEQ 1(
              ECHO PC-13 2 >> result.txt
     )   
 )ELSE(
-     ECHO PC-13 2 result.txt
+     ECHO PC-13 2 >> result.txt
 )
 
 DEL LocalSecurityPolicy.txt
