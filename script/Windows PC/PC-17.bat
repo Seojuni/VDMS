90fef9a808e2d60511e12764f54e5a330be2d0bc
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-15 14:38:12 +0900

    Update PC-17.bat

--- script/Windows PC/PC-17.bat
+++ script/Windows PC/PC-17.bat
@@ -3,7 +3,7 @@
 REM[PC-17] (중) 2.서비스 관리 > 2.5 대상 시스템이 Windows 서버를 제외한 다른 OS로 멀티부틸이 가능하지 않도록 설정
 
 
-secedit /EXPORT /CFG LocalSeurityPoilicy.txt
+secedit /EXPORT /CFG LocalSeurityPolicy.txt
 TYPE LocalSecurityPolicy.txt | find -i "MULTI_BOOTING" >> multi.txt
 
 IF %MULTI_BOOTING% EQU 1 (
