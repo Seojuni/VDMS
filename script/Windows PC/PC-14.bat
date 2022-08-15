ac89a0632ae849e703503f7cef9482dfd4112248
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-15 14:36:42 +0900

    Update PC-14.bat

--- script/Windows PC/PC-14.bat
+++ script/Windows PC/PC-14.bat
@@ -2,7 +2,7 @@
 
 REM[PC-14] (상) 4.보안관리 > 4.6 PC내부의 미사용(3개월) ActiveX 제거
 
-secedit /EXPORT /CFG LocalSeurityPoilicy.txt
+secedit /EXPORT /CFG LocalSeurityPolicy.txt
 TYPE LocalSecurityPolicy.txt | find -i "ActiveX_INSPECTION" >> inspection.txt
 TYPE LocalSecurityPolicy.txt | find -i "ActiveX_Delete" >>delete.txt
 
@@ -12,12 +12,12 @@ rem 1 : 양호 /2: 취약
 
 IF %ActiveX_INSPECTION% EQU 1(
     IF %ActiveX_Delete% EQU 1(
-            ECHO PC-14 1 result.txt
+            ECHO PC-14 1 >> result.txt
     )ELSE(
-            ECHO PC-14 2 result.txt
+            ECHO PC-14 2 >> result.txt
     )
 )ELSE(
-    ECHO PC-14 2 result.txt
+    ECHO PC-14 2 >> result.txt
 )
 
 DEL LocalSecurityPolicy.txt
