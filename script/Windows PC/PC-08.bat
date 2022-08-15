4d511d579b39ff4dba39400083a4a1c8112f3903
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-15 14:33:03 +0900

    Update PC-08.bat

--- script/Windows PC/PC-08.bat
+++ script/Windows PC/PC-08.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM[PC-08] (상) 3. 패치관리 > 3.3 MS-Office, 한글, 어도비 등의 으용 프로그램에 대한 최신 보안 패치 및 벤더 권고사항 적용
 
-secedit /EXPORT /CFG LocalSeurityPoilicy.txt
+secedit /EXPORT /CFG LocalSeurityPolicy.txt
 
 TYPE LocalSecurityPolicy.txt | find -i "LATEST_PATCHES" >> lastes.txt
 TYPE LocalSecurityPolicy.txt | find -i "managementProcedure" >> manage_pro.txt
@@ -15,6 +15,6 @@ IF %LATEST_PATCHES% EQU 1(
  ECHO PC-08 2 >> result.txt
 )
 
-DEL LocalSeurityPoilicy.txt
+DEL LocalSeurityPolicy.txt
 DEL lastes.txt
 DEL manage_pro.txt
