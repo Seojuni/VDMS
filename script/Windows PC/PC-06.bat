eb6e028ee2020d31e8dfaf5f9e1c556b23a5afb8
HeeSeonKang <109217369+HeeSeonKang@users.noreply.github.com>
2022-08-15 14:34:12 +0900

    Update PC-06.bat

--- script/Windows PC/PC-06.bat
+++ script/Windows PC/PC-06.bat
@@ -1,7 +1,7 @@
 @ECHO OFF
 REM[PC-06] (상) 3. 패치 관리 > 3.1 HOT FIX 등 최신 보안패치 적용
 
-secedit /EXPORT /CFG LocalSeurityPoilicy.txt
+secedit /EXPORT /CFG LocalSeurityPolicy.txt
 
 TYPE LocalSecurityPolicy.txt | find -i "HOT_FIX" >> HOT__FIX.txt
 TYPE LocalSecurityPolicy.txt | find -i "managementProcedure" >> manage_pro.txt
@@ -16,6 +16,6 @@ IF %HOT_FIX% EQU 1(
     ELSE ECHO PC-06 2 >> result.txt
 )ELSE ECHO PC-06 >> result.txt
 
-DEL LocalSeurityPoilicy.txt
+DEL LocalSeurityPolicy.txt
 DEL HOT_FIX.txt
 DEL manage_pro.txt
