49aefb7f294faa3c1f0dced44bc06993c2c19fca
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-16 10:05:25 +0900

    Update vuln_comparison.py

--- vuln_comparison.py
+++ vuln_comparison.py
@@ -21,7 +21,7 @@ class App():
             print('Failed to open URL. Unsupported variable type.')
 
     def get_software_list(self):
-        get_software_list_query = 'select name, version from software_list'
+        get_software_list_query = """select name, version from software_list"""
         try:
             self.cur.execute(get_software_list_query)
         except mariadb.Error as e:
@@ -30,8 +30,8 @@ class App():
 
 
     def vuln_comparison(self):
-        get_cve_list_query = 'select * from cve_list'
-        update_query = 'update software_list set vuln_check = %s, vuln_id = %s where name = %s'
+        get_cve_list_query = """select * from cve_list"""
+        insert_query = """insert into vuln_list(name, version, vuln_id) values (?, ?, ?)"""
         try:
             self.cur.execute(get_cve_list_query)
         except mariadb.Error as e:
@@ -42,7 +42,7 @@ class App():
                 if row[5].lower() in software[0].lower():
                     if version.parse(row[6]) <= version.parse(software[1]) <= version.parse(row[7]):
                         try:
-                            self.cur.execute(update_query, ('1', row[0]), software[0])
+                            self.cur.execute(insert_query, (software[0], software[1], row[0]))
                             self.conn.commit()
 
                             self.vuln_id = row[0]
@@ -57,18 +57,7 @@ class App():
                             )
                         except mariadb.Error as e:
                             print(f"Error: {e}")
-                    else:
-                        try:
-                            self.cur.execute(update_query, ('0', None, software[0]))
-                            self.conn.commit()
-                        except mariadb.Error as e:
-                            print(f"Error: {e}")
-                else:
-                    try:
-                        self.cur.execute(update_query, ('0', None, software[0]))
-                        self.conn.commit()
-                    except mariadb.Error as e:
-                        print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     app = App()
