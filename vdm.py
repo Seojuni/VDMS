6b4e3ffe8776ff9f40b9ff40517633b799717576
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-17 20:49:05 +0900

    Update vdm.py

--- vdm.py
+++ vdm.py
@@ -58,7 +58,7 @@ class App(customtkinter.CTk):
 
         # ============ frame_left ============
         self.frame_left.grid_rowconfigure(0, minsize=10)   # empty row with minsize as spacing
-        self.frame_left.grid_rowconfigure(6, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(7, weight=1)  # empty row as spacing
         self.frame_left.grid_rowconfigure(8, minsize=20)    # empty row with minsize as spacing
         self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
 
@@ -82,9 +82,14 @@ class App(customtkinter.CTk):
         self.button_2.grid(row=4, column=0, pady=10, padx=20)
 
         self.button_3 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="Vulnerability List",
+                                                command=self.vuln_button_event)
+        self.button_3.grid(row=5, column=0, pady=10, padx=20)
+
+        self.button_4 = customtkinter.CTkButton(master=self.frame_left,
                                                 text="Dashboard",
                                                 command=self.three_button_event)
-        self.button_3.grid(row=5, column=0, pady=10, padx=20)
+        self.button_4.grid(row=6, column=0, pady=10, padx=20)
 
         self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="Appearance Mode:")
         self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
@@ -113,8 +118,8 @@ class App(customtkinter.CTk):
                                                    justify=tkinter.LEFT)
         self.label_info.grid(column=0, row=0, sticky="nwe", padx=15, pady=15)
 
-        t = threading.Thread(target=self.cve_crawler)
-        t.start()
+        cve_crawler = threading.Thread(target=self.cve_crawler)
+        cve_crawler.start()
 
         # set default values
         self.optionmenu_1.set("Dark")
@@ -126,6 +131,9 @@ class App(customtkinter.CTk):
         while True:
             schedule.run_pending()
 
+    def vuln_comparison(self):
+        os.system("pythonw -u vuln_comparison.py > log.log 2>&1")
+
     # go to vulnerability diagnosis
     def one_button_event(self):
         self.frame_one = customtkinter.CTkFrame(master=self.frame_right)
@@ -134,7 +142,7 @@ class App(customtkinter.CTk):
         self.frame_one.rowconfigure(0, weight=1)
         self.frame_one.columnconfigure(0, weight=1)
 
-        my_os = self.get_os()
+        self.my_os = self.get_os()
         date = self.get_date()
 
         s = ttk.Style()
@@ -146,10 +154,10 @@ class App(customtkinter.CTk):
         tree.grid()
 
         tree["columns"] = ["one"]
-        tree.heading("#0", text='취약점 진단(' + my_os + ')')
+        tree.heading("#0", text='취약점 진단(' + self.my_os + ')')
         tree.heading("#1", text=date)
 
-        if my_os == "Windows 10":
+        if self.my_os == "Windows PC":
             tree.insert('', tkinter.END, text='계정 관리', iid=1, open=False)
             tree.insert('', tkinter.END, text='서비스 관리', iid=2, open=False)
             tree.insert('', tkinter.END, text='패치 관리', iid=3, open=False)
@@ -207,12 +215,112 @@ class App(customtkinter.CTk):
                 for j in range(len(sec)):
                     tree.move(code, 4, j)
 
+        elif self.my_os == "Windows Server":
+            tree.insert('', tkinter.END, text='계정 관리', iid=1, open=False)
+            tree.insert('', tkinter.END, text='서비스 관리', iid=2, open=False)
+            tree.insert('', tkinter.END, text='패치 관리', iid=3, open=False)
+            tree.insert('', tkinter.END, text='로그 관리', iid=4, open=False)
+            tree.insert('', tkinter.END, text='보안 관리', iid=5, open=False)
+            tree.insert('', tkinter.END, text='DB 관리', iid=6, open=False)
+
+            acc = ['[W-01] (상) Administrator 계정 이름 변경 또는 보안성 강화', '[W-02] (상) Guest 계정 비활성화', '[W-03] (상) 불필요한 계정 제거',
+                   '[W-04] (상) 계정 잠금 임계값 설정', '[W-05] (상) 해독 가능한 암호화를 사용하여 암호 저장 해제', '[W-06] (상) 관리자 그룹에 최소한의 사용자 포함',
+                   '[W-46] (중) Everyone 사용권한을 익명 사용자에 적용 해제', '[W-47] (중) 계정 잠금 기간 설정', '[W-48] (중) 패스워드 복잡성 설정',
+                   '[W-49] (중) 패스워드 최소 암호 길이', '[W-50] (중) 패스워드 최대 사용 기간', '[W-51] (중) 패스워드 최소 사용 기간',
+                   '[W-52] (중) 마지막 사용자 이름 표시 안함', '[W-53] (중) 로컬 로그온 허용', '[W-54] (중) 익명 SID/이름 변환 허용 해제',
+                   '[W-55] (중) 최근 암호 기억', '[W-56] (중) 콘솔 로그온 시 로컬 계정에서 빈 암호 사용 제한', '[W-57] (중) 원격터미널 접속 가능한 사용자 그룹 제한']
+
+            for i in range(len(acc)):
+                code = acc[i].split(' ')[0]
+                tree.insert('', tkinter.END, text=acc[i], iid=code, open=False)
+
+            for i in range(len(acc)):
+                code = acc[i].split(' ')[0]
+                for j in range(len(acc)):
+                    tree.move(code, 1, j)
+
+            ser = ['[W-07] (상) 공유 권한 및 사용자 그룹 설정', '[W-08] (상) 하드디스크 기본 공유 제거', '[W-09] (상) 불필요한 서비스 제거',
+                   '[W-10] (상) IIS 서비스 구동 점검', '[W-11] (상) IIS 디렉토리 리스팅 제거', '[W-12] (상) IIS CGI 실행 제한',
+                   '[W-13] (상) IIS 상위 디렉토리 접근 금지', '[W-14] (상) IIS 불필요한 파일 제거', '[W-15] (상) IIS 웹프로세스 권한 제한',
+                   '[W-16] (상) IIS 링크 사용 금지', '[W-17] (상) IIS 파일 업로드 및 다운로드 제한', '[W-18] (상) IIS DB 연결 취약점 점검',
+                   '[W-19] (상) IIS 가상 디렉토리 삭제', '[W-20] (상) IIS 데이터파일 ACL 적용', '[W-21] (상) IIS 미사용 스크립트 매핑 제거',
+                   '[W-22] (상) IIS Exec 명령어 쉘 호출 진단', '[W-23] (상) IIS WebDAV 비활성화', '[W-24] (상) NetBIOS 바인딩 서비스 구동 점검',
+                   '[W-25] (상) FTP 서비스 구동 점검', '[W-26] (상) FTP 디렉토리 접근 권한 설정', '[W-27] (상) Anonymous FTP 금지',
+                   '[W-28] (상) FTP 접근 제어 설정', '[W-29] (상) DNS Zone Transfer 설정', '[W-30] (상) RDS(Remonte Data Services) 제거',
+                   '[W-31] (상) 최신 서비스팩 적용', '[W-58] (중) 터미널 서비스 암호화 수준 설정', '[W-59] (중) IIS 웹 서비스 정보 숨김',
+                   '[W-60] (중) SNMP 서비스 구동 점검', '[W-61] (중) SNMP 서비스 커뮤니티스트링의 복잡성 설정', '[W-62] (중) SNMP Access control 설정',
+                   '[W-63] (중) DNS 서비스 구동 점검', '[W-64] (하) HTTP/FTP/SMTP 배너 차단', '[W-65] (중) Telnet 보안 설정',
+                   '[W-66] (중) 불필요한 ODBC/OLE=DB 데이터소스와 드라이브 제거', '[W-67] (중) 원격터미널 접속 타임아웃 설정',
+                   '[W-68] (중) 예약된 작업에 의심스러운 명령이 등록되어 있는지 점검']
+
+            for i in range(len(ser)):
+                code = ser[i].split(' ')[0]
+                tree.insert('', tkinter.END, text=ser[i], iid=code, open=False)
+
+            for i in range(len(ser)):
+                code = ser[i].split(' ')[0]
+                for j in range(len(ser)):
+                    tree.move(code, 2, j)
+
+            patch = ['[W-32] (상) 최신 HOT FIX 적용', '[W-33] (상) 백신 프로그램 업데이트', '[W-69] (중) 정책에 따른 시스템 로깅설정']
+
+            for i in range(len(patch)):
+                code = patch[i].split(' ')[0]
+                tree.insert('', tkinter.END, text=patch[i], iid=code, open=False)
+
+            for i in range(len(patch)):
+                for j in range(len(patch)):
+                    code = patch[i].split(' ')[0]
+                    tree.move(code, 3, j)
+
+            log = ['[W-34] (상) 로그의 정기적 검토 및 보고', '[W-35] (상) 원격으로 엑세스 할 수 있는 레지스트리 경로',
+                   '[W-70] (하) 이벤트 로그 관리 설정', '[W-71] (중) 원격에서 이벤트 로그파일 접근 차단']
+
+            for i in range(len(log)):
+                code = log[i].split(' ')[0]
+                tree.insert('', tkinter.END, text=log[i], iid=code, open=False)
+
+            for i in range(len(log)):
+                for j in range(len(log)):
+                    code = log[i].split(' ')[0]
+                    tree.move(code, 4, j)
+
+            sec = ['[W-36] (상) 백신 프로그램 설치', '[W-37] (상) SAM 파일 접근 통제 설정', '[W-38] (상) 화면보호기 설정',
+                   '[W-39] (상) 로그온 하지않고 시스템 종료 허용 해제', '[W-40] (상) 원격 시스템에서 강제로 시스템 종료',
+                   '[W-41] (상) 보안감사를 로그할 수 없는 경우 즉시 시스템 종료 해제', '[W-42] (상) SAM 계정과 공유의 익명 열거 허용 안함',
+                   '[W-43] (상) Autologon 기능 제어', '[W-44] (상) 이동식 미디어 포맷 및 꺼내기 허용', '[W-45] (상) 디스크 볼륨 암호화 설정',
+                   '[W-72] (중) Dos 공격 방어 레지스트리 설정', '[W-73] (중) 사용자가 프린터 드라이버를 설치할 수 없게 함',
+                   '[W-74] (중) 세션 연결을 중단하기 전에 필요한 유휴시간', '[W-75] (하) 경고 메시지 설정', '[W-76] (중) 사용자별 홈 디렉토리 권한 설정',
+                   '[W-77] (중) LAN Manager 인증 수준', '[W-78] (중) 보안 채널 데이터 디지털 암호화 또는 서명', '[W-79] (중) 파일 및 디렉토리 보호',
+                   '[W-80] (중) 컴퓨터 계정 암호 최대 사용 기간', '[W-81] (중) 시작 프로그램 목록 분석']
+
+            for i in range(len(sec)):
+                code = sec[i].split(' ')[0]
+                tree.insert('', tkinter.END, text=sec[i], iid=code, open=False)
+
+            for i in range(len(sec)):
+                code = sec[i].split(' ')[0]
+                for j in range(len(sec)):
+                    tree.move(code, 5, j)
+
+            db = ['[W-82] (중) Windows 인증 모드 사용']
+
+            for i in range(len(db)):
+                code = db[i].split(' ')[0]
+                tree.insert('', tkinter.END, text=db[i], iid=code, open=False)
+
+            for i in range(len(db)):
+                code = db[i].split(' ')[0]
+                for j in range(len(db)):
+                    tree.move(code, 6, j)
+
         tree.grid(row=0, column=0, sticky='nsew')
 
         self.button_start = customtkinter.CTkButton(master=self.frame_one,
-                                                text="Start",
-                                                command=lambda: self.vuln_diagnosis(tree))
+                                                    text="Start",
+                                                    command=lambda: self.vuln_diagnosis(tree))
         self.button_start.grid(row=1, column=0, pady=10, padx=20)
+
 
     # go to cve list
     def two_button_event(self):
@@ -293,6 +401,72 @@ class App(customtkinter.CTk):
         item = self.treeview.selection()[0]
         webbrowser.open('https://nvd.nist.gov/vuln/detail/'+self.treeview.item(item, "values")[0])
 
+    #go to Vulnerability List
+    def vuln_button_event(self):
+        self.frame_vuln = customtkinter.CTkFrame(master=self.frame_right)
+        self.frame_vuln.grid(row=0, column=0, sticky="nsew")
+
+        self.frame_vuln.rowconfigure(0, weight=1)
+        self.frame_vuln.columnconfigure(0, weight=1)
+        self.label_vuln = customtkinter.CTkLabel(master=self.frame_vuln,
+                                                text="Vulnerability LIST",
+                                                height=20,
+                                                corner_radius=6,  # <- custom corner radius
+                                                fg_color=("white", "gray38"),  # <- custom tuple-color
+                                                justify=tkinter.LEFT)
+        self.label_vuln.pack(fill="x", padx=15, pady=15)
+
+        s = ttk.Style()
+        s.theme_use("clam")
+        s.configure("Treeview.Heading", font=(None, 14), rowheight=int(14 * 10))
+        s.configure("Treeview", font=(None, 12), rowheight=int(12 * 5))
+
+        self.vuln_treeview = tkinter.ttk.Treeview(self.frame_vuln,
+                                             height=50,
+                                             columns=["name", "version", "vuln_id"],
+                                             displaycolumns=["name", "version", "vuln_id"]
+                                             )
+        ysb = ttk.Scrollbar(self.frame_vuln, orient="vertical", command=self.vuln_treeview.yview)
+        ysb.pack(side='right', fill='y', pady=15)
+        self.vuln_treeview.configure(yscrollcommand=ysb.set)
+        self.vuln_treeview.pack(fill="x", padx=15, pady=15)
+
+        self.vuln_treeview.column("#0", width=40)
+        self.vuln_treeview.heading("#0", text="Num")
+        self.vuln_treeview.column("name", width=150, anchor="center")
+        self.vuln_treeview.heading("name", text="Software Name", anchor="center")
+        self.vuln_treeview.column("version", width=150, anchor="center")
+        self.vuln_treeview.heading("version", text="Software Version", anchor="center")
+        self.vuln_treeview.column("vuln_id", width=150, anchor="center")
+        self.vuln_treeview.heading("vuln_id", text="CVE Code", anchor="center")
+
+        self.vuln_treeview.bind("<Double-1>", self.vuln_OnDoubleClick)
+
+        conn = db_conn.db_conn()
+        cur = conn.cursor()
+
+        select_query = "select * from vuln_list"
+
+        try:
+            cur.execute(select_query)
+        except mariadb.Error as e:
+            print(f"Error: {e}")
+
+        resultset = cur.fetchall()
+
+        for i, data in enumerate(resultset):
+            self.vuln_treeview.insert("", tkinter.END, text=i + 1, values=data, iid=data[0])
+
+        conn.close()
+
+        # vuln_comparison = threading.Thread(target=self.vuln_comparison)
+        # vuln_comparison.start()
+
+    # view cve detail webpage
+    def vuln_OnDoubleClick(self, event):
+        item = self.vuln_treeview.selection()[0]
+        webbrowser.open('https://nvd.nist.gov/vuln/detail/' + self.vuln_treeview.item(item, "values")[2])
+
     # go to Dashboard
     def three_button_event(self):
         self.frame_main = customtkinter.CTkFrame(master=self.frame_right)
@@ -317,7 +491,12 @@ class App(customtkinter.CTk):
 
         reg.CloseKey(open)
 
-        return " ".join(str(value).split(' ')[0:2])
+        os = " ".join(str(value).split(' ')[0:2])
+
+        if os == "Windows 10" or os == "Windows 11" or os == "Windows 8":
+            return "Windows PC"
+        else:
+            return "Windows Server"
 
     def get_date(self):
         week = datetime.date.today().weekday()
@@ -339,7 +518,6 @@ class App(customtkinter.CTk):
         self.frame_one.columnconfigure(0, weight=1)
 
         selected = tree.get_checked()
-        print(selected)
         flg = 0
         self.digdate = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
@@ -365,8 +543,8 @@ class App(customtkinter.CTk):
         for item in selected:
             self.append_log('Start diagnosing the ' + item[1:-1])
 
-            #subprocess.call(['C:\\Users\\USER\\Desktop\\vdm\\script\\Windows PC\\' + item[1:-1] + '.bat'])
-            print('C:\\Users\\USER\\Desktop\\vdm\\script\\Windows PC\\' + item[1:-1] + '.bat')
+            subprocess.call(['C:\\Users\\USER\\Desktop\\vdm\\script\\' + self.my_os + '\\' + item[1:-1] + '.bat'])
+            #print('C:\\Users\\USER\\Desktop\\vdm\\script\\' + self.my_os + '\\' + item[1:-1] + '.bat')
 
             self.progressbar.set((selected.index(item)+1) / len(selected) * 1.0)
             self.append_log(item[1:-1] + ' has been diagnosed.')
