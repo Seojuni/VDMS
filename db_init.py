0c4de7114018be88520b2c8c4d155df15c71547c
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-18 22:09:40 +0900

    Update db_init.py

--- db_init.py
+++ db_init.py
@@ -5,7 +5,7 @@ import db_conn
 def db_init():
     conn = db_conn.db_conn()
     cur = conn.cursor()
-    
+
     try:
         cur.execute("""create table cve_list (
             vuln_id VARCHAR(15) NOT NULL PRIMARY KEY,
@@ -19,25 +19,23 @@ def db_init():
             cwe_id VARCHAR(20) NOT NULL,
             cwe_name VARCHAR(100) NOT NULL);""")
         conn.commit()
-    
+
         cur.execute("""create table vuln_list (
-            No INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
+            name VARCHAR(100) NOT NULL PRIMARY KEY,
             version VARCHAR(30) NOT NULL,
             vuln_id VARCHAR(15));""")
         conn.commit()
-    
+
         cur.execute("""create table software_list (
-            No INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-            name VARCHAR(100) NOT NULL,
+            name VARCHAR(100) NOT NULL PRIMARY KEY,
             version VARCHAR(30) NOT NULL);""")
         conn.commit()
-    
+
         cur.execute("""create table diagnostic_results(
             type_code VARCHAR(5) NOT NULL PRIMARY KEY,
             result VARCHAR(1) NOT NULL);""")
         conn.commit()
     except mariadb.Error as e:
         print(f"Error: {e}")
-    
+
     conn.close()
