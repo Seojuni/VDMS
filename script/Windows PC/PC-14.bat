4cb8e544cfbdf9752f8d30ec3a1a9ea8aa3b3c67
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-19 00:36:37 +0900

    Update PC-14.bat

--- script/Windows PC/PC-14.bat
+++ script/Windows PC/PC-14.bat
@@ -1,22 +1,19 @@
 @ECHO OFF
 
-REM[PC-14] (상) 4.보안관리 > 4.6 PC내부의 미사용(3개월) ActiveX 제거
+rem [PC-14] (상) 4.보안관리 > 4.6 PC내부의 미사용(3개월) ActiveX 제거
 
 secedit /EXPORT /CFG LocalSeurityPolicy.txt
-TYPE LocalSecurityPolicy.txt | find -i "ActiveX_INSPECTION" >> inspection.txt
-TYPE LocalSecurityPolicy.txt | find -i "ActiveX_Delete" >>delete.txt
+TYPE LocalSecurityPolicy.txt | find -i "ActiveX_INSPECTION" > inspection.txt
+TYPE LocalSecurityPolicy.txt | find -i "ActiveX_Delete" > delete.txt
 
-rem 1 : 양호 /2: 취약
-
-
-
-IF %ActiveX_INSPECTION% EQU 1(
-    IF %ActiveX_Delete% EQU 1(
-            ECHO PC-14 1 >> result.txt
-    )ELSE(
-            ECHO PC-14 2 >> result.txt
+rem 1 : 양호 / 2 : 취약
+IF %ActiveX_INSPECTION% EQU 1 (
+    IF %ActiveX_Delete% EQU 1 (
+        ECHO PC-14 1 >> result.txt
+    ) ELSE (
+        ECHO PC-14 2 >> result.txt
     )
-)ELSE(
+) ELSE (
     ECHO PC-14 2 >> result.txt
 )
 
