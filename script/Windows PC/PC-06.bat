c6e8ee8629e15f40f33d6fa8a44cfa8e7006d9c2
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-19 00:20:32 +0900

    Update PC-06.bat

--- script/Windows PC/PC-06.bat
+++ script/Windows PC/PC-06.bat
@@ -1,20 +1,20 @@
 @ECHO OFF
-REM[PC-06] (상) 3. 패치 관리 > 3.1 HOT FIX 등 최신 보안패치 적용
+
+rem [PC-06] (상) 3. 패치 관리 > 3.1 HOT FIX 등 최신 보안패치 적용
 
 secedit /EXPORT /CFG LocalSeurityPolicy.txt
+TYPE LocalSecurityPolicy.txt | find -i "HOT_FIX" > HOT__FIX.txt
+TYPE LocalSecurityPolicy.txt | find -i "managementProcedure" > manage_pro.txt
 
-TYPE LocalSecurityPolicy.txt | find -i "HOT_FIX" >> HOT__FIX.txt
-TYPE LocalSecurityPolicy.txt | find -i "managementProcedure" >> manage_pro.txt
-
-rem 1: 양호/ 2: 취약
-
-
-IF %HOT_FIX% EQU 1(
-    IF %managementProcedure% EQU 1(
+rem 1 : 양호 / 2 : 취약
+IF %HOT_FIX% EQU 1 (
+    IF %managementProcedure% EQU 1 (
         ECHO PC-06 1 >> result.txt
-    )
-    ELSE ECHO PC-06 2 >> result.txt
-)ELSE ECHO PC-06 >> result.txt
+    ) ELSE (
+        ECHO PC-06 2 >> result.txt
+) ELSE (
+    ECHO PC-06 2 >> result.txt
+)
 
 DEL LocalSeurityPolicy.txt
 DEL HOT_FIX.txt
