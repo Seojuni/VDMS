e1164c43b3c53d5d8445383a7c3cbf1a92736b43
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-19 00:32:45 +0900

    Update PC-11.bat

--- script/Windows PC/PC-11.bat
+++ script/Windows PC/PC-11.bat
@@ -1,20 +1,18 @@
 @ECHO OFF
-REM[PC-11] (상) 4. 보안관리 > 4.3 OS에서 제공하는 침입차단 기능 활성화
+rem [PC-11] (상) 4. 보안관리 > 4.3 OS에서 제공하는 침입차단 기능 활성화
 
 secedit /EXPORT /CFG LocalSeurityPolicy.txt
-TYPE LocalSecurityPolicy.txt | find -i "WINDOWS_FIREWALL" >> windows.txt
-TYPE LocalSecurityPolicy.txt | find -i "OTHER_FIREWALL" >>other.txt
+TYPE LocalSecurityPolicy.txt | find -i "WINDOWS_FIREWALL" > windows.txt
+TYPE LocalSecurityPolicy.txt | find -i "OTHER_FIREWALL" > other.txt
 
-rem 1 : 양호 /2: 취약
-
-
-IF %WINDOWS_FIREWALL% EQU 1(
-    IF %PAID_FIREWALL% EQU 1
+rem 1 : 양호 / 2 : 취약
+IF %WINDOWS_FIREWALL% EQU 1 (
+    IF %PAID_FIREWALL% EQU 1 (
         ECHO PC-11 1 >> result.txt
-    )ELSE(
+    ) ELSE (
         ECHO PC-11 2 >> result.txt
     )
-)ELSE(
+) ELSE (
     ECHO PC-11 2 >> result.txt
 )
 
