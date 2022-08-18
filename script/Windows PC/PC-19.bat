ca502c639210804934259ef83b4700e0331e59b7
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-19 00:40:36 +0900

    Update PC-19.bat

--- script/Windows PC/PC-19.bat
+++ script/Windows PC/PC-19.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 
-REM[PC-19] (중) 4.보안관리 > 4.7 원격 지우너을 금지하도록 정책 설정
+rem [PC-19] (중) 4.보안관리 > 4.7 원격 지우너을 금지하도록 정책 설정
 
 secedit /EXPORT /CFG LocalSeurityPolicy.txt
+TYPE LocalSecurityPolicy.txt | find -i "REMOTE_PROCEDURE" > remote.txt
 
-TYPE LocalSecurityPolicy.txt | find -i "REMOTE_PROCEDURE" >> remote.txt
-
+rem 1 : 양호 / 2 : 취약
 IF %REMOTE_PROCEDURE% NEQ 1 (
     ECHO PC-19 1 >> result.txt
-)ELSE(
+) ELSE (
     ECHO PC-19 2 >> resuilt.txt
 )
 
