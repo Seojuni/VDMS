58e0710e91e2b8e9052e0793df2ee0e929da6f7a
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-19 00:37:15 +0900

    Update PC-15.bat

--- script/Windows PC/PC-15.bat
+++ script/Windows PC/PC-15.bat
@@ -1,16 +1,14 @@
 @ECHO OFF
 
-REM[PC-15] (중) 1. 계정관리 > 1.3 복구 콘솔에서 자동 로그온을 금지하도록 설정
+rem [PC-15] (중) 1. 계정관리 > 1.3 복구 콘솔에서 자동 로그온을 금지하도록 설정
 
 secedit /EXPORT /CFG LocalSeurityPolicy.txt
-TYPE LocalSecurityPolicy.txt | find -i "Recovery_Console" >> recovery.txt
+TYPE LocalSecurityPolicy.txt | find -i "Recovery_Console" > recovery.txt
 
-
-rem 1 : 양호 /2: 취약
-
+rem 1 : 양호 / 2 : 취약
 IF %Recovery_Console% NEQ 1 (
     ECHO PC-15 1 >> result.txt
-)ELSE(
+) ELSE (
     ECHO PC-15 2 >> result.txt
 )
 
