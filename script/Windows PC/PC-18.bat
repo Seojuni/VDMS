46af3eac7e5fbf72f2b796deb1f898310ebd304b
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-19 00:39:59 +0900

    Update PC-18.bat

--- script/Windows PC/PC-18.bat
+++ script/Windows PC/PC-18.bat
@@ -1,14 +1,13 @@
 @ECHO OFF
 
-REM[PC-18] (하) 2.서비스 관리 > 2.6 브라우저 종료 시 임시 인터넷 파일 폴더의 내용을 삭제하도록 설정
+rem [PC-18] (하) 2.서비스 관리 > 2.6 브라우저 종료 시 임시 인터넷 파일 폴더의 내용을 삭제하도록 설정
 
 secedit /EXPORT /CFG LocalSeurityPolicy.txt
+TYPE LocalSecurityPolicy.txt | find -i "EMPTY_FOLDER" > empty.txt
 
-TYPE LocalSecurityPolicy.txt | find -i "EMPTY_FOLDER" >> empty.txt
-
-IF %EMPTY_FOLDER% EQU 1(
+IF %EMPTY_FOLDER% EQU 1 (
     ECHO PC-18 1 >> result.txt
-)ELSE (
+) ELSE (
     ECHO PC-18 2 >> result.txt
 )
 
