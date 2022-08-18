2ea275880d2ab9cd8b7e397ad1a3bbfeb009f304
Seojuni <109189956+Seojuni@users.noreply.github.com>
2022-08-19 00:23:55 +0900

    Update PC-07.bat

--- script/Windows PC/PC-07.bat
+++ script/Windows PC/PC-07.bat
@@ -1,16 +1,19 @@
 @ECHO OFF
+
+rem [PC-07] (상) 3. 패치 관리 > 3.2 최신 서비스팩 적용
+
 secedit /EXPORT /CFG LocalSecurityPolicy.txt
+TYPE LocalSecurityPolicy.txt | find -i "LATEST_SERVICES" > lastes.txt
+TYPE LocalSecurityPolicy.txt | find -i "managementProcedure" > manage_pro.txt
 
-TYPE LocalSecurityPolicy.txt | find -i "LATEST_SERVICES" >> lastes.txt
-TYPE LocalSecurityPolicy.txt | find -i "managementProcedure" >> manage_pro.txt
-
-IF %LATEST_SERVICES% EQU 1(
-    IF %managementProcedure% EQU 1(
+IF %LATEST_SERVICES% EQU 1 (
+    IF %managementProcedure% EQU 1 (
         ECHO PC-07 1 >> result.txt
-    ) ELSE ECHO PC-07 2 >> result.txt
-)ELSE(
-
- ECHO PC-07 2 >> result.txt
+    ) ELSE (
+        ECHO PC-07 2 >> result.txt
+    )
+) ELSE (
+    ECHO PC-07 2 >> result.txt
 )
 
 DEL LocalSeurityPolicy.txt
